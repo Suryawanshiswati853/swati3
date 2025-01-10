1e67c9db97b273b385455abf7c54432d9749ca04
suryawanshiswati3101 <suryawanshiswati3101@gmail.com>
2025-01-10 16:54:25 +0530

    my new project

--- new_project2/lib/main.dart
+++ new_project2/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:new_project2/post/screens/post_screen.dart';
-import 'package:new_project2/post/screens/wallet/wallet_screen.dart';
+import 'package:new_project2/screens/wallet/wallet_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
